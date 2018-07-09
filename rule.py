9e57ebb95e63649b3d12b3c659e1791eab2ab2a0
lucas <lucas@thestrangeagency.com>
2018-07-09 10:45:04 +0200

    generates and displays initial state

--- rule.py
+++ rule.py
@@ -2,6 +2,10 @@ import argparse
 
 
 import sys
+from random import randint
+
+
+WIDTH = 64
 
 
 def parse(args):
@@ -12,11 +16,24 @@ def parse(args):
     return parser.parse_args(args)
 
 
+def print_state(state):
+    print("".join('x' if x else '.' for x in state))
+
+
 def main(args):
     args = parse(args)
+    
     rule = str(bin(args.rule))[2:].zfill(8)
     print('rule: {}'.format(rule))
     
+    n = randint(0, 2 ** WIDTH)
+    state = [False for i in range(WIDTH)]
+    for i in range(WIDTH):
+        state[(WIDTH-1) - i] = True if n & 1 else False
+        n = n >> 1
+    
+    print_state(state)
+
 
 if __name__ == '__main__':
     main(sys.argv[1:])
